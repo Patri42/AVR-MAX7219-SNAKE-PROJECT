f82097ca0c12b31da42949dae8350a9c36894e91
Patri42 <patrick.borzych@gmail.com>
2023-08-04 13:45:43 +0200

    define DRead, DWrite, pinmode

--- main.c
+++ main.c
@@ -6,3 +6,7 @@
 #define VERT_PIN PC0 //  A0
 #define HORZ_PIN PC1 //  A1
 #define SEL_PIN PD2  //  D2
+
+#define digitalRead(pin) (!!(PIND & (1 << pin))) // Only for PORTD
+#define digitalWrite(pin, value) (value ? (PORTD |= (1 << pin)) : (PORTD &= ~(1 << pin))) // Only for PORTD
+#define pinMode(pin, mode) (mode == OUTPUT ? (DDRD |= (1 << pin)) : (DDRD &= ~(1 << pin))) // Only for PORTD
