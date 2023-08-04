6a34a6f073bd3e05a556babe91273832fb1aa137
Patri42 <patrick.borzych@gmail.com>
2023-08-04 18:20:10 +0200

    mian v2

--- main.c
+++ main.c
@@ -20,63 +20,30 @@
 
 //#define MAX_DEVICES	1
 
-#define VERT_THRESHOLD_LOW 300
-#define VERT_THRESHOLD_HIGH 700
-#define HORZ_THRESHOLD_LOW 300
-#define HORZ_THRESHOLD_HIGH 700
+uint16_t readAnalog(uint8_t pin)
+{
+    ADMUX = (ADMUX & 0xF8) | (pin & 0x07); // select the ADC channel
+    ADCSRA |= (1 << ADSC); // start the conversion
+    while (ADCSRA & (1 << ADSC)); // wait for the conversion to finish
+    return ADCW; // return the ADC value
+}
+
+void adc_init()
+{
+    ADMUX = (1 << REFS0); // reference voltage on AVCC
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // ADC enabled, prescaler division=128 (16Mhz/128=125Khz)
+}
 
 int x = 0;
 int y = 0;
 
-int readAnalog(uint8_t pin) {
-    // TODO: write code here to read from the ADC
-    return 0;  // placeholder return value
-}
-
-void setPoint(int x, int y) {
-    // Clear the display buffer
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            max7219b_clr(i, j);
-    }
-}
-
-    // Set the point in the buffer
-    max7219b_set(x, y);
-    // Output the buffer to the display
-    max7219b_out();
-}
-
 int main ()
-    {
-        init_serial();
-        max7219_init();
+{
+    init_serial();
+    max7219_init();
+    adc_init();
 
-        pinMode(VERT_PIN, INPUT);
-	    pinMode(HORZ_PIN, INPUT);
-	    pinMode(SEL_PIN, INPUT_PULLUP);
-
-
-        while (1) {  // main loop
-            int vert = readAnalog(VERT_PIN);
-            int horz = readAnalog(HORZ_PIN);
-            if (vert < VERT_THRESHOLD_LOW) {
-                y = y > 0 ? y - 1 : 0;
-            }
-            if (vert > VERT_THRESHOLD_HIGH) {
-                y = y < 7 ? y + 1 : 7;
-            }
-            if (horz < HORZ_THRESHOLD_LOW) {
-                x = x > 0 ? x - 1 : 0;
-            }
-            if (horz > HORZ_THRESHOLD_HIGH) {
-                x = x < 7 ? x + 1 : 7;
-            }
-            if (!digitalRead(SEL_PIN)) {
-                x = y = 0;
-            }
-            setPoint(x, y);
-            _delay_ms(100);
-        }
-    return 0;
-    }
+    pinMode(VERT_PIN, INPUT);
+    pinMode(HORZ_PIN, INPUT);
+    pinMode(SEL_PIN, INPUT_PULLUP);
+}
