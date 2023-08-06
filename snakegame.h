0d7a2b735ee7a27c9c3fd30a9602baee335d783c
Patri42 <patrick.borzych@gmail.com>
2023-08-06 12:27:31 +0200

    init game

--- snakegame.h
+++ snakegame.h
@@ -4,6 +4,14 @@
 #include "snake.h"
 
 typedef struct {
+    Snake* snake;           // The snake object
+    int foodX;              // X coordinate of the food
+    int foodY;              // Y coordinate of the food
+    int score;              // Current score
+    bool isGameOver;        // Flag to check if the game is over
+} Game;
+
+typedef struct {
     int x;
     int y;
 } Point;
